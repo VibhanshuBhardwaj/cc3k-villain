81fdfacfdde052888c1df0d06cded634acbdbf3c
rutpatel <rutpatel010@gmail.com>
2017-04-01 19:45:59 -0400

    Update Enemy.h

--- Character/Enemy/Enemy.h
+++ Character/Enemy/Enemy.h
@@ -2,9 +2,9 @@
 #define _ENEMY_H_
 #include "../Character.h"
 
-#include "string"
+#include <string>
 #include "../Player/Player.h"
-#include "vector"
+#include <vector>
 
 class Enemy : public Character {
 	bool isHostile;
